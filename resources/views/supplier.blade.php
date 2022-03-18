5bc884bc513f3ee83ba860ff5e8ba7377c8e1e90
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-18 20:30:33 +0700

    show layout view for supplier

---
+++ resources/views/supplier.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.master')
+@section('content')
+
+<h2 align="center">Daftar Supplier</h2>
+    <div class="container mt-3">
+        <table class="table table-success table-striped">
+            <thead>
+                <tr>
+                  <th scope="col">No</th>
+                  <th scope="col">Gambar</th>
+                  <th scope="col">Nama</th>
+                  <th scope="col">Alamat</th>
+                  <th scope="col">Nomor Telp</th>
+                  <th scope="col">Deskripsi</th>
+                </tr>
+              </thead>
+              <tbody>
+                @foreach ($suppliers as $supplier)
+                <tr>
+                    <th scope="row">{{$supplier->id}}</th>
+                    <td>
+                        <img src="{{asset('img/profil/supplier.png')}}" alt="" height="50px">
+                    </td>
+                    <td>{{$supplier -> nama}}</td>
+                    <td>{{$supplier -> alamat}}</td>
+                    <td>{{$supplier -> notelp}}</td>
+                    <td>{{$supplier -> deskripsi}}</td>
+                  </tr>
+                @endforeach
+              </tbody>
+          </table>
+    </div>
+
+@endsection
