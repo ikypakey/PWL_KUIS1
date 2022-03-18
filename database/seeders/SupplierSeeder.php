205458187d3d3908f70cece3193a73fd282e6878
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-18 20:19:22 +0700

    make seeder for supplier

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
 
 class SupplierSeeder extends Seeder
 {
@@ -13,6 +15,52 @@ class SupplierSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('pegawais')->insert(
+            [
+                [
+                    'nama' => 'CV. Mustika Jaya ',
+                    'alamat' => 'Surabaya',
+                    'notelp' => '089765423455',
+                    'deskripsi' => 'Supplier Buah '
+                ],
+                [
+                    'nama' => 'CV. Harapan Bangsa ',
+                    'alamat' => 'Mojokerto',
+                    'notelp' => '082265423455',
+                    'deskripsi' => 'Supplier Sayur'
+                ],
+                [
+                    'nama' => 'CV. Meat Sejahtera ',
+                    'alamat' => 'Surabaya',
+                    'notelp' => '081165423455',
+                    'deskripsi' => 'Supplier Daging Sapi'
+                ],
+                [
+                    'nama' => 'CV. Ayam Mandiri ',
+                    'alamat' => 'Pasuruan',
+                    'notelp' => '080965423455',
+                    'deskripsi' => 'Supplier Daging Ayam'
+                ],
+                [
+                    'nama' => 'CV. Goat Nusantara ',
+                    'alamat' => 'Surabaya',
+                    'notelp' => '089762113455',
+                    'deskripsi' => 'Supplier Daging Kambing'
+                ],
+                [
+                    'nama' => 'CV. Sehat Sejahtera ',
+                    'alamat' => 'Sidoarjo',
+                    'notelp' => '089763333455',
+                    'deskripsi' => 'Supplier Susu'
+                ],
+                  [
+                    'nama' => 'CV. Indo Makmur ',
+                    'alamat' => 'Sidoarjo',
+                    'notelp' => '081163333455',
+                    'deskripsi' => 'Supplier Bumbu'
+                ],
+                
+            ]
+            );
     }
 }
