ac6cf229cc647f6f2386dcd4e152489b5c3595ce
masbarok193@gmail.com <masbarok193@gmail.com>
2022-03-18 15:26:35 +0700

    Fix Assets

--- resources/views/blog-details.blade.php
+++ resources/views/blog-details.blade.php
@@ -54,7 +54,7 @@
     <!-- Hero Section End -->
 
     <!-- Blog Details Hero Begin -->
-    <section class="blog-details-hero set-bg" data-setbg="img/blog/details/details-hero.jpg">
+    <section class="blog-details-hero set-bg" data-setbg="{{asset('img/blog/details/details-hero.jpg')}}">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -99,7 +99,7 @@
                             <div class="blog__sidebar__recent">
                                 <a href="#" class="blog__sidebar__recent__item">
                                     <div class="blog__sidebar__recent__item__pic">
-                                        <img src="img/blog/sidebar/sr-1.jpg" alt="">
+                                        <img src="{{asset('img/blog/sidebar/sr-1.jpg')}}" alt="">
                                     </div>
                                     <div class="blog__sidebar__recent__item__text">
                                         <h6>09 Kinds Of Vegetables<br /> Protect The Liver</h6>
@@ -108,7 +108,7 @@
                                 </a>
                                 <a href="#" class="blog__sidebar__recent__item">
                                     <div class="blog__sidebar__recent__item__pic">
-                                        <img src="img/blog/sidebar/sr-2.jpg" alt="">
+                                        <img src="{{asset('img/blog/sidebar/sr-2.jpg')}}" alt="">
                                     </div>
                                     <div class="blog__sidebar__recent__item__text">
                                         <h6>Tips You To Balance<br /> Nutrition Meal Day</h6>
@@ -117,7 +117,7 @@
                                 </a>
                                 <a href="#" class="blog__sidebar__recent__item">
                                     <div class="blog__sidebar__recent__item__pic">
-                                        <img src="img/blog/sidebar/sr-3.jpg" alt="">
+                                        <img src="{{asset('img/blog/sidebar/sr-3.jpg')}}" alt="">
                                     </div>
                                     <div class="blog__sidebar__recent__item__text">
                                         <h6>4 Principles Help You Lose <br />Weight With Vegetables</h6>
@@ -141,7 +141,7 @@
                 </div>
                 <div class="col-lg-8 col-md-7 order-md-1 order-1">
                     <div class="blog__details__text">
-                        <img src="img/blog/details/details-pic.jpg" alt="">
+                        <img src="{{asset('img/blog/details/details-pic.jpg')}}" alt="">
                         <p>Sed porttitor lectus nibh. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet
                             dui. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Mauris blandit
                             aliquet elit, eget tincidunt nibh pulvinar a. Vivamus magna justo, lacinia eget consectetur
@@ -162,7 +162,7 @@
                             <div class="col-lg-6">
                                 <div class="blog__details__author">
                                     <div class="blog__details__author__pic">
-                                        <img src="img/blog/details/details-author.jpg" alt="">
+                                        <img src="{{asset('img/blog/details/details-author.jpg')}}" alt="">
                                     </div>
                                     <div class="blog__details__author__text">
                                         <h6>Michael Scofield</h6>
@@ -207,7 +207,7 @@
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <div class="blog__item">
                         <div class="blog__item__pic">
-                            <img src="img/blog/blog-1.jpg" alt="">
+                            <img src="{{asset('img/blog/blog-1.jpg')}}" alt="">
                         </div>
                         <div class="blog__item__text">
                             <ul>
@@ -222,7 +222,7 @@
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <div class="blog__item">
                         <div class="blog__item__pic">
-                            <img src="img/blog/blog-2.jpg" alt="">
+                            <img src="{{asset('img/blog/blog-2.jpg')}}" alt="">
                         </div>
                         <div class="blog__item__text">
                             <ul>
@@ -237,7 +237,7 @@
                 <div class="col-lg-4 col-md-4 col-sm-6">
                     <div class="blog__item">
                         <div class="blog__item__pic">
-                            <img src="img/blog/blog-3.jpg" alt="">
+                            <img src="{{asset('img/blog/blog-3.jpg')}}" alt="">
                         </div>
                         <div class="blog__item__text">
                             <ul>
