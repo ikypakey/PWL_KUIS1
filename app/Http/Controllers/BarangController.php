f34db63d3ef6b42e14b8ce3c0f3e0318668ebcba
danimaulana25 <ahmaddanimaulana80@gmail.com>
2022-03-18 18:30:19 +0700

    configure BarangController

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Barang;
 class BarangController extends Controller
 {
-    //
+    public function shopgrid(){
+        return view('shop-grid',[
+            'title' => 'Barang',
+            'barangs' => Barang::all()
+        ]);
+    }
 }
