00fd29dd0df5570bf6a3ef6baea4d1b2b7098292
danimaulana25 <ahmaddanimaulana80@gmail.com>
2022-03-21 10:47:32 +0700

    fix pagination on barang

--- resources/views/shop-grid.blade.php
+++ resources/views/shop-grid.blade.php
@@ -404,22 +404,16 @@
                         @endforeach
                         
                     </div>
-                    <div class="product__pagination">
-                        <a href="#">1</a>
-                        <a href="#">2</a>
-                        <a href="#">3</a>
-                        <a href="#"><i class="fa fa-long-arrow-right"></i></a>
-                    </div>
+                    <div class="row">
+                        <ul class="pagination pagination justify-content-center">
+                             {{ $barangs->links() }}
+                         </ul>
+                    </div>                
                 </div>
             </div>
         </div>
     </section>
     <!-- Product Section End -->
-    <div class="row">
-        <ul class="pagination pagination justify-content-center">
-             {{ $barangs->links() }}
-         </ul>
-    </div>
-
+    
 
 @endsection
