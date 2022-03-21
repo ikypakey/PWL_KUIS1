f6fc7d01d86eb029fd9456b668bee4fdaef5ab2b
danimaulana25 <ahmaddanimaulana80@gmail.com>
2022-03-21 10:58:10 +0700

    fix data on table barangs coloum harga

--- database/seeders/BarangSeeder.php
+++ database/seeders/BarangSeeder.php
@@ -19,62 +19,62 @@ class BarangSeeder extends Seeder
                 [
                     'gambar' => 'img/product/product-1.jpg',
                     'nama' => 'Daging ',
-                    'harga' => '$30.00'
+                    'harga' => 30.00
                 ],
                 [
                     'gambar' => 'img/product/product-2.jpg',
                     'nama' => 'Pisang ',
-                    'harga' => '$25.00'
+                    'harga' => 25.00
                 ],
                 [
                     'gambar' => 'img/product/product-3.jpg',
                     'nama' => 'Jambu Merah',
-                    'harga' => '$15.00'
+                    'harga' => 15.00
                 ],
                 [
                     'gambar' => 'img/product/product-4.jpg',
                     'nama' => 'Anggur',
-                    'harga' => '$17.00'
+                    'harga' => 17.00
                 ],
                 [
                     'gambar' => 'img/product/product-5.jpg',
                     'nama' => 'Hamburger',
-                    'harga' => '$22.00'
+                    'harga' => 22.00
                 ],
                 [
                     'gambar' => 'img/product/product-6.jpg',
                     'nama' => 'Mangga',
-                    'harga' => '$22.00'
+                    'harga' => 22.00
                 ],
                 [
                     'gambar' => 'img/product/product-7.jpg',
                     'nama' => 'Semangka',
-                    'harga' => '$26.00'
+                    'harga' => 26.00
                 ],
                 [
                     'gambar' => 'img/product/product-8.jpg',
                     'nama' => 'Apel',
-                    'harga' => '$16.00'
+                    'harga' => 16.00
                 ],
                 [
                     'gambar' => 'img/product/product-9.jpg',
                     'nama' => 'Sliwki',
-                    'harga' => '$19.00'
+                    'harga' => 19.00
                 ],
                 [
                     'gambar' => 'img/product/product-10.jpg',
                     'nama' => 'Ayam kripsi',
-                    'harga' => '$21.00'
+                    'harga' => 21.00
                 ],
                 [
                     'gambar' => 'img/product/product-11.jpg',
                     'nama' => 'Tropicana',
-                    'harga' => '$21.00'
+                    'harga' => 21.00
                 ],
                 [
                     'gambar' => 'img/product/product-12.jpg',
                     'nama' => 'Buah buahan',
-                    'harga' => '$45.00'
+                    'harga' => 45.00
                 ],
             ]
             );
